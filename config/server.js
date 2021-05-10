60c1c71710758162d5e598fb8c077df9b98061e7
tngeene <tngeene27@gmail.com>
2021-05-10 15:07:09 +0300

    setup models

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e724f152f059f80fa5c8a3ad61273772'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3823669-cb84-4507-86e3-cb567846cf9e'
+};
